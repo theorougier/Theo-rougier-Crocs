c1cb0d667c92b70d89c22fedca9deca4719cb6a3
Theo Rougier <theo_roougier@hotmail.fr>
2019-12-11 10:03:50 +0000

    Mise en ligne

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Theo-rougier-Crocs
+# hetic-w1p2022-02
+
+Téo Lugat
+Samuel Sebastiao
+Dorian Vidal
+Lucas Massol
+Emmy Peteytas
+Théo Rougier
+Kyllian Mendy
+
+https://dreamy-bhaskara-d10658.netlify.com/
---
+++ burger.js
@@ -0,0 +1,5 @@
+let menuIcon = document.getElementById("hamburger-menu");
+let navbar = document.getElementById("navbar");
+navbar.addEventListener("click", () => {
+  navbar.classList.toggle("change");
+});
